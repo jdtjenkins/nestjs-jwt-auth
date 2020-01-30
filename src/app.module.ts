94f93eafb6a50e1af3a4035d0e4e1913e88e7f81
Jacob Jenkins <jacob@rotacloud.com>
2020-01-30 13:37:18 +0000

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+// Controllers
+import { AppController } from './app.controller';
+
+// Services
+import { AppService } from './app.service';
+
+@Module({
+	imports: [
+		TypeOrmModule.forRoot({
+			type: 'mysql',
+			host: 'localhost',
+			port: 3306,
+			username: 'root',
+			password: 'example',
+		}),
+	],
+	controllers: [AppController],
+	providers: [AppService],
+})
+export class AppModule { }
