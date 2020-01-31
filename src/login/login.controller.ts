b5c0b8a3de57c4cfdef366812df64e86d5d3b1ee
Jacob Jenkins <jacob@rotacloud.com>
2020-01-31 13:40:35 +0000

    moves sending password by basic auth

--- src/login/login.controller.ts
+++ src/login/login.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
+import { Controller, Request, Post, UseGuards, Get, Headers } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from '../auth/auth.service';
 
@@ -8,8 +8,11 @@ export class LoginController {
 
 	@UseGuards(AuthGuard('local'))
 	@Post('auth/login')
-	async login(@Request() req) {
-		return this.authService.login(req.user);
+	async login(@Headers('authorization') auth: string) {
+		const encodedToken = auth.split('Basic ')[1];
+		const [username, password] = Buffer.from(encodedToken, 'base64').toString().split(':');
+
+		return this.authService.login(username, password);
 	}
 
 	@UseGuards(AuthGuard('jwt'))
